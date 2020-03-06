1e39c486aef9370108b1d45c7394265737d3a111
Jessan Cherian <jessan.cherian@gmail.com>
2020-03-06 02:25:53 -0800

    adminpage + remove image from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,6 @@ class User < ApplicationRecord
              user.password = Devise.friendly_token[0,20]
              user.first_name = auth.info.first_name
              user.last_name = auth.info.last_name
-             user.image = auth.info.image # assuming the user model has an image
              user.save!
            end
          end
