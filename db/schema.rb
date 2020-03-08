4be02c4a9471349489360dc0d1c3d0fbacdb6c65
Jessan Cherian <jessan.cherian@gmail.com>
2020-03-07 18:11:39 -0800

    image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200306221712) do
+ActiveRecord::Schema.define(version: 20200308015316) do
 
   create_table "active_admin_comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "namespace"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20200306221712) do
     t.string "last_name"
     t.string "provider"
     t.string "uid"
+    t.text "image_data"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
